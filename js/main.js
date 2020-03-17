56c7d9d7943e000605558cdaeb5650b45c9bf91f
Michael Hernandez <gammabeta010@gmail.com>
2020-03-17 22:45:28 +0100

    Finito codice per ogni card con API

--- js/main.js
+++ js/main.js
@@ -3,29 +3,34 @@ $(document).ready(function() {
     var source = $('#card-template').html();
     var template = Handlebars.compile(source);
 
+    console.log(template);
+
     $.ajax({
         url : 'https://flynn.boolean.careers/exercises/api/array/music',
         method : 'GET',
         success: function(data) {
-            console.log(data.response);
-            currObj.html(data.response);
+            //console.log(data.response);
 
-            if(data.response <= 5) {
-                if(currObj.hasClass('green')) {
-                    currObj.removeClass('green')
+            var albumList = data.response;
+
+            for (var i = 0; i < albumList.length; i++) {
+                //console.log(albumList[i]);
+
+                var album = {
+                    imgAlbum: albumList[i].poster,
+                    albumName: albumList[i].title,
+                    artist: albumList[i].author,
+                    genre: albumList[i].genre,
+                    year: albumList[i].year
                 }
 
-                currObj.addClass('yellow');
-            }
-            else {
-                if(currObj.hasClass('yellow')) {
-                    currObj.removeClass('yellow')
-                }
-                currObj.addClass('green');
+                var cardAlbum = template(album);
+                $('.card-block').append(cardAlbum);
+
             }
         },
         error: function() {
-        alert('Failed')
+        alert('Failed to call API')
         }
 
 
