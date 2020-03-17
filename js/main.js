e392edb1f737e728e89083ea4e6bb0a118194060
Michael Hernandez <gammabeta010@gmail.com>
2020-03-17 21:45:10 +0100

    Finito header e un card di prova.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
 
+    var source = $('#card-template').html();
+    var template = Handlebars.compile(source);
+
+    $.ajax({
+        url : 'https://flynn.boolean.careers/exercises/api/array/music',
+        method : 'GET',
+        success: function(data) {
+            console.log(data.response);
+            currObj.html(data.response);
+
+            if(data.response <= 5) {
+                if(currObj.hasClass('green')) {
+                    currObj.removeClass('green')
+                }
+
+                currObj.addClass('yellow');
+            }
+            else {
+                if(currObj.hasClass('yellow')) {
+                    currObj.removeClass('yellow')
+                }
+                currObj.addClass('green');
+            }
+        },
+        error: function() {
+        alert('Failed')
+        }
+
+
+    });
 
 
 
-    
 })
